8fbeb8bdf4820ad55c45d62c7572537c106b7a61
jaychakr <158199926+jaychakr@users.noreply.github.com>
2024-10-06 15:21:44 -0700

    Test Data

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -31,7 +31,9 @@ class QuestionAdmin(admin.ModelAdmin):
 class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
+
 # <HINT> Register Question and Choice models here
+
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
 admin.site.register(Instructor)
